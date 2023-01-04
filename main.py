3d410c6d4a44441ec6ab00e7435159e0a14fdf60
Safaith Hasan <90445473+Safaith@users.noreply.github.com>
2023-01-04 20:47:43 +0600

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-num1 = 2 + 5j
-print(num1.real, num1.imag)
-item = ["safaith", "hasan", 7, True]
-item.append("judah")
-item.extend(["sane", "mount"])
-item.remove("hasan")
-item.pop()
-item.insert(3, "halland")
-print(item)
